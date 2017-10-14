c826d0020ae26eb563c6b1bca18c62c8112d1591
Archon <archonshen@gmail.com>
2017-10-14 20:02:19 +0800

    add serializer

--- src/org/gp3/moblima/Runner.java
+++ src/org/gp3/moblima/Runner.java
@@ -1,16 +1,26 @@
 package org.gp3.moblima;
 
+import org.gp3.moblima.model.Model;
 import org.gp3.moblima.model.Movie;
-import org.gp3.moblima.model.SerializeModel;
+
+import java.util.ArrayList;
 
 public class Runner {
     public static void main (String [] args)
     {
         Movie movie = new Movie();
         movie.TID=3664645;
-        SerializeModel.writeSerial(movie);
-        movie = (Movie)SerializeModel.readSerial();
-        System.out.println(movie.TID);
+        Movie movie1=new Movie();
+        movie1.TID=123123;
+        ArrayList<Model> temp = new ArrayList<>();
+        temp.add(movie);
+        temp.add(movie1);
+
+        Model.writeSerial(temp);
+
+        temp = Model.readSerial();
+        System.out.println(((Movie)temp.get(0)).TID);
+        System.out.println(((Movie)temp.get(1)).TID);
 
     }
 }
--- src/org/gp3/moblima/model/Model.java
+++ src/org/gp3/moblima/model/Model.java
@@ -1,9 +1,56 @@
 package org.gp3.moblima.model;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.util.ArrayList;
+
 /**
- * Created by Archonsh on 14/10/17.
+ * Created by Shen Youlin on 14/10/17.
  */
+
+
 public class Model implements java.io.Serializable
 {
+    public static void writeSerial(ArrayList<Model> models)
+    {
+        try
+        {
+            FileOutputStream fileOut = new FileOutputStream("123.ser",false);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
 
+            out.writeObject(models);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved\n");
+        }
+        catch(IOException i)
+        {
+            i.printStackTrace();
+        }
+    }
+    @Nullable
+    public static ArrayList<Model> readSerial()
+    {
+        ArrayList<Model> models = null;
+        try
+        {
+            FileInputStream fileIn = new FileInputStream("123.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            models = (ArrayList<Model>) in.readObject();
+            in.close();
+            fileIn.close();
+            return models;
+        }catch(IOException i)
+        {
+            i.printStackTrace();
+            return null;
+        }catch(ClassNotFoundException c)
+        {
+            System.out.println("class not found");
+            c.printStackTrace();
+            return null;
+        }
+
+    }
 }
