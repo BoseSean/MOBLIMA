1396755afb4ba43e293d92d0f9ba9eb394077453
hannancao <caomichael19@gmail.com>
2017-10-15 00:42:18 +0800

    add mainmenu

--- src/org/gp3/moblima/view/MainMenu.java
+++ src/org/gp3/moblima/view/MainMenu.java
@@ -5,11 +5,34 @@ import java.util.Scanner;
  * created by hannancao on 15/10/17.
  */
 public class MainMenu {
+    private StaffMenu mStaffMenu;
+    private MovieGoerMenu mMovieGoerMenu;
+    private CineplexManager mCineplexManager;
+
     public void displayMenu({
         int choice = 0;
         while(choice != 3){
             Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+            if(choice == 1)
+                mStaffMenu.displayMenu();
+            else if(choice == 2)
+                mMovieGoerMenu.displayMenu();
+            else
+                break;
+        }
+    }
 
+    public MainMenu(){
+        try{
+            mCineplexManager = new CineplexManager();
+
+            mStaffMenu = new StaffMenu(mCineplexManager);
+            mCineplexManager = new MovieGoerMenu(mCineplexManager);
+        }
+        catch(Exception e){
+            System.out.println("Ooops, problem with loading data. Please contact the administrator");
+            e.printStackTrace();
         }
     }
 }
