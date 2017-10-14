7c50d83d94e358d8954ba5463dcec35ed2048295
hannancao <caomichael19@gmail.com>
2017-10-15 00:31:21 +0800

    add mainmenu

---
+++ src/org/gp3/moblima/view/MainMenu.java
@@ -0,0 +1,14 @@
+package org.gp3.moblima.view;
+
+import java.util.Scanner;
+/**
+ * created by hannancao on 15/10/17.
+ */
+public class MainMenu {
+    public void displayMenu({
+        int choice = 0;
+        while(choice != 3){
+            Scanner sc = new Scanner(System.in);
+        }
+    }
+}
