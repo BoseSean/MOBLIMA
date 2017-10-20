2bbd5e0a9cd303ced3612fb9786eabb68c991405
BoseSean <xinyesg@gmail.com>
2017-10-21 01:39:19 +0800

    Edited ListMoviesMenu

--- src/org/gp3/moblima/view/moviegoer/ListMoviesMenu.java
+++ src/org/gp3/moblima/view/moviegoer/ListMoviesMenu.java
@@ -1,15 +1,13 @@
 package org.gp3.moblima.view.moviegoer;
 
-import java.util.ArrayList;
-
 import org.gp3.moblima.controller.Manager;
 import org.gp3.moblima.model.Movie;
 import org.gp3.moblima.view.BaseMenu;
 import org.gp3.moblima.view.Quit;
 
-import static org.gp3.moblima.view.IOUtil.printMenuItems;
-import static org.gp3.moblima.view.IOUtil.printTitle;
-import static org.gp3.moblima.view.IOUtil.readChoice;
+import java.util.ArrayList;
+
+import static org.gp3.moblima.view.IOUtil.*;
 
 /**
  * Created by zhangxinye on 19/10/17.
@@ -24,36 +22,34 @@ public class ListMoviesMenu extends BaseMenu {
     public BaseMenu execute() {
         printTitle("Movie Listing");
 		Manager mg = Manager.getInstance();
-		ArrayList<Movie> movie = mg.getAll("MOVIE");
 
 
-		for (Movie movies : movie) {
-			movies.getInfo();
-		}
 		ArrayList<String> choices = new ArrayList<>();
-        choices.add("All Movie");
+		ArrayList<Movie> movies = mg.getAll("MOVIE");
+		for (Movie m : movies) {
+			choices.add(m.getTitle());
+		}
+
         choices.add("Show Top 5 by sale");
         choices.add("Shop Top 5 by rating");
+		choices.add("Back");
 		choices.add("Quit");
 		printMenuItems(choices, 0);
 
 		int c = readChoice("Choice (0-" + choices.size() + ") : ", choices.size());
 
 		BaseMenu nextMenu = this;
-		switch (c) {
-			case 0:
-//				nextMenu = new MovieInfo(this, movie);
-				break;
-			case 1:
-				nextMenu = new ListMovieTopSale(this);
-				break;
-			case 2:
-				nextMenu = new ListMovieTopRate(this);
-				break;
-			case 3:
-				nextMenu = this.getPreviousMenu();
-				break;
-		}
+		if (c < movies.size())
+			nextMenu = new MovieInfo(this, movies.get(c));
+		else if (c == movies.size())
+			nextMenu = new ListMovieTopSale(this.getPreviousMenu());
+		else if (c == movies.size() + 1)
+			nextMenu = new ListMovieTopRate(this.getPreviousMenu());
+		else if (c == movies.size() + 2)
+			nextMenu = this.getPreviousMenu();
+		else if (c == movies.size() + 3)
+			nextMenu = new Quit(null);
+
 		return nextMenu;
     }
 
