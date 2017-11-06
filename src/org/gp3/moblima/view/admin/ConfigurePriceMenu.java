db9bd3382c5fb365d50e4ca8bf219af49892c614
hannancao <caomichael19@gmail.com>
2017-11-06 21:20:33 +0800

    edit ConfigurePrice again

--- src/org/gp3/moblima/view/admin/ConfigurePriceMenu.java
+++ src/org/gp3/moblima/view/admin/ConfigurePriceMenu.java
@@ -1,6 +1,7 @@
 package org.gp3.moblima.view.admin;
 
 import org.gp3.moblima.controller.Manager;
+import org.gp3.moblima.controller.PriceManager;
 import org.gp3.moblima.model.Holiday;
 import org.gp3.moblima.model.TicketPrice;
 import org.gp3.moblima.view.BaseMenu;
@@ -17,53 +18,72 @@ import java.util.Date;
 import static org.gp3.moblima.model.Constant.Tables.HOLIDAY;
 
 import static org.gp3.moblima.model.Constant.Tables.TICKET;
-import static org.gp3.moblima.model.Constant.Tables.TICKETPRICE;
 import static org.gp3.moblima.view.IOUtil.*;
 import static org.gp3.moblima.view.IOUtil.println;
 
 public class ConfigurePriceMenu extends BaseMenu {
+    private PriceManager priceManager;
+
 
     public ConfigurePriceMenu(BaseMenu previousMenu) {
         super(previousMenu);
     }
-
 
     @Override
     public BaseMenu execute() {
         // TODO by DAXIONGDI
         printTitle("Configure Price Menu");
 
-        Manager manager = PriceManager.getInstance();
-
+        priceManager = PriceManager.getInstance();
+        boolean toUpdatePlatinum;
+        boolean toUpdateSnake;
+        MovieType movieTypeToChanhge;
+        TicketType ticketTypeToChange;
         ArrayList<String> choices = new ArrayList<String>();
 
-        TicketPrice ticketprice = new TicketPrice();
-
-        choices.add("Update Price");
-        choices.add("Add New Price");
-
-        printMenuItems(choices, 0);
-
+        println("Do you want to change ticket price for Platinum or Not Platinum?");
+        choices.add("Platinum");
+        choices.add("Not Platinum");
         int c = readChoice(0, choices.size());
-        switch(c){
-            case 0:
-                String name = read("Input Ticket type");
-                ticketprice = manager.getEntry(TICKETPRICE, (TicketPrice h) -> (h.equals(name));
-                if(ticket != null) {
-                    int price = readInt("Input Ticket price");
-                }
-                break;
-            case 1:
-                name = read("Input corresponding ticket type to be remove: ");
-                ticket = manager.getEntry(TICKET, (Ticket h) -> (h.equals(name)));
-                if (ticket != null) {
-                    manager.delete(TICKET, ticket);
-                } else {
-                    println("Ticket type Does Not Exists");
-                }
-                break;
-            case 2:
+        switch (c) {
+            case 0: toUpdatePlatinum=true;break;
+            case 1: toUpdatePlatinum=false;break;
         }
+
+        println("Do you want to change ticket price for Snake or No Snake?");
+        choices.clear();
+        choices.add("Snake");
+        choices.add("No Snake");
+        c = readChoice(0, choices.size());
+        switch (c) {
+            case 0: toUpdateSnake=true;break;
+            case 1: toUpdateSnake=false;break;
+        }
+
+        println("What kind of movie to change price?");
+        choices.clear();
+        for (MovieType type : MovieType.values()){
+            choices.add(type.toString());
+        }
+        c = readChoice(0, choices.size());
+        movieTypeToChanhge = MovieType.values()[c];
+
+        println("What kind of ticket to change price?");
+        choices.clear();
+        for (TicketType type : TicketType.values()){
+            choices.add(type.toString());
+        }
+        c = readChoice(0, choices.size());
+        ticketTypeToChange = TicketType.values()[c];
+
+
+
         return this.getPreviousMenu();
     }
+
+    private void updatePrice(){
+        String name = read("Input Ticket type");
+
+    }
+
 }
