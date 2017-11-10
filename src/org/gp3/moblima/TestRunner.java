900cc25a4abd306e292110cd52b15d1df2011cb5
wudaown <wudaown@gmail.com>
2017-11-10 21:39:22 +0800

    add db

--- src/org/gp3/moblima/TestRunner.java
+++ src/org/gp3/moblima/TestRunner.java
@@ -12,11 +12,12 @@ public class TestRunner {
     public static void main(String[] args) {
         Manager manager = Manager.getInstance();
 
-        manager.add(Constant.Tables.MOVIE, new Movie("Matrix"));
-        manager.add(Constant.Tables.ADMIN, new Admin("admin", "admin"));
-        manager.add(Constant.Tables.MOVIE, new Movie(""));
-        manager.add(Constant.Tables.USER, new User("user", "12312312312", "user@email.com"));
-        manager.add(Constant.Tables.CINEMA, new Cinema("haha",false, new ArrayList<Slot>()));
+//        manager.add(Constant.Tables.MOVIE, new Movie("Matrix"));
+//        manager.add(Constant.Tables.ADMIN, new Admin("admin", "admin"));
+//        manager.add(Constant.Tables.MOVIE, new Movie(""));
+//        manager.add(Constant.Tables.USER, new User("user", "12312312312", "user@email.com"));
+//        manager.add(Constant.Tables.CINEMA, new Cinema("JEM",false, new ArrayList<Slot>()));
+//        manager.add(Constant.Tables.CINEMA, new Cinema("West Mall",false, new ArrayList<Slot>()));
         MenuLoop loop = new MenuLoop(new MainMenu(null));
         loop.start();
 
