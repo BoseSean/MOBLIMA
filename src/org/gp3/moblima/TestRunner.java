1cf72c1766d012e3ac3ac19f41fca33541a6987a
wudaown <wudaown@gmail.com>
2017-11-15 23:25:37 +0800

    fix holiday and more

--- src/org/gp3/moblima/TestRunner.java
+++ src/org/gp3/moblima/TestRunner.java
@@ -4,8 +4,6 @@ import org.gp3.moblima.controller.Manager;
 import org.gp3.moblima.view.MainMenu;
 import org.gp3.moblima.view.MenuLoop;
 
-import static org.gp3.moblima.view.IOUtil.*;
-
 import java.util.Date;
 
 public class TestRunner {
@@ -16,6 +14,7 @@ public class TestRunner {
 //        manager.add(Constant.Tables.USER, new User("user", "12312312312", "user@email.com"));
 //        manager.add(Constant.Tables.CINEMA, new Cinema("JEM", false, new ArrayList<Slot>(), "JEM",Constant.Cineplex.JEM,10,10));
 //        manager.add(Constant.Tables.CINEMA, new Cinema("West Mall", false, new ArrayList<Slot>(), "WEM",Constant.Cineplex.WEST_MALL,10, 10));
+
 
         MenuLoop loop = new MenuLoop(new MainMenu(null));
         loop.start();
--- src/org/gp3/moblima/view/admin/ConfigureHolidayMenu.java
+++ src/org/gp3/moblima/view/admin/ConfigureHolidayMenu.java
@@ -1,6 +1,7 @@
 package org.gp3.moblima.view.admin;
 
 import org.gp3.moblima.controller.Manager;
+import org.gp3.moblima.model.Constant;
 import org.gp3.moblima.model.Holiday;
 import org.gp3.moblima.view.BaseMenu;
 
@@ -37,61 +38,59 @@ public class ConfigureHolidayMenu extends BaseMenu {
 
 		int c = readChoice(0, choices.size());
 
+		ArrayList<Holiday> holidays = manager.getAll(Constant.Tables.HOLIDAY);
+		ArrayList<String> Hochoices = new ArrayList<>();
+		for (Holiday h : holidays) {
+			Hochoices.add(h.getName());
+		}
+		int hc = 0;
+
+		Date startdate = null;
 		switch (c) {
 			case 0:
-				String name = read("Input Holiday Name");
-				holiday = manager.getEntry(HOLIDAY, (Holiday h) -> (h.getName().equals(name)));
-				if (holiday != null) {
-					String newName = read("Input New Holiday Name: ");
-					holiday.setName(newName);
-					Date startdate = readDate("Please new Date: ");
-					holiday.setDate(startdate);
-//					String date = read("Input New Date (DD/MM): ");
-//					date = date + "/2017";
-//					DateFormat df = new SimpleDateFormat("DD/MM/YYYY");
-//					Date startDate;
-//					try {
-//						startDate = df.parse(date);
-//						holiday.setDate(startDate);
-//					} catch (ParseException e) {
-//						e.printStackTrace();
-//					} finally {
-//						println("Update Done");
-//					}
+//				String name = read("Input Holiday Name: ");
+				printMenuItems(Hochoices, 0);
 
-				} else {
-					println("Holiday Does Not Exists");
-				}
+				println("Select Holiday to be updated: ");
+
+				hc = readChoice(0, Hochoices.size());
+
+				holiday = holidays.get(hc);
+
+
+
+//				holiday = manager.getEntry(HOLIDAY, (Holiday h) -> (h.getName().equals(name)));
+//				if (holiday != null) {
+				String newName = read("Input New Holiday Name: ");
+				holiday.setName(newName);
+				startdate = readDate("Please new Date: ");
+				holiday.setDate(startdate);
+
+//				} else {
+//					println("Holiday Does Not Exists");
+//				}
 				break;
 			case 1:
-				name = read("Input Holiday to be remove: ");
-				holiday = manager.getEntry(HOLIDAY, (Holiday h) -> (h.getName().equals(name)));
-				if (holiday != null) {
-					manager.delete(HOLIDAY, holiday);
-				} else {
-					println("Holiday Does Not Exists");
-				}
+//				name = read("Input Holiday to be remove: ");
+//				holiday = manager.getEntry(HOLIDAY, (Holiday h) -> (h.getName().equals(name)));
+//				if (holiday != null) {
+				println("Select Holiday to be removed: ");
+				printMenuItems(Hochoices, 0);
+
+				hc = readChoice(0, Hochoices.size());
+
+				holiday = holidays.get(hc);
+				manager.delete(HOLIDAY, holiday);
+//				} else {
+//					println("Holiday Does Not Exists");
+//				}
 				break;
 			case 2:
-				name = read("Input New Holiday Name: ");
+				String name = read("Input New Holiday Name: ");
 				holiday.setName(name);
-				Date startdate = readDate("Please input Date: ");
+				startdate = readDate("Please input Date: ");
 				holiday.setDate(startdate);
 				manager.add(HOLIDAY, holiday);
-//				String date = read("Input New Date (DD/MM): ");
-//				date = date + "/2017";
-//				DateFormat df = new SimpleDateFormat("DD/MM/YYYY");
-//				Date startDate;
-//				try {
-//					startDate = df.parse(date);
-//					holiday.setDate(startDate);
-//					manager.add(HOLIDAY, holiday);
-//				} catch (ParseException e) {
-//					e.printStackTrace();
-//				}
-//				finally {
-//					println("New Holiday Added");
-//				}
 				break;
 		}
 
